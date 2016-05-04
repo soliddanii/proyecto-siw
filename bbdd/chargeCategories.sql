f09f04d81993f630d1a2f59578a9823c10c87900
Daniel Catalán (soliddanii) <soliddanii3@gmail.com>
2016-05-04 19:24:38 +0200

    Cambios en las tablas SQL
    He echo cambios que arreglan algunas tablas y las acercan mas al diseño propuesto en el documento. Ejemplos: el nombre en la tabla usuario puede ser NULL porque no hace falta rellenarlo; la tabla categoria tiene ahora un campo ID que es su clave primaria; Ajustados algunos nombres de atributos, y longitud de variables; Añadidos comentarios sobre las tablas; La tabla comentario ahora tiene el campo fecha que le faltaba,  tiene un nuevo atributo ID que hace de clave primaria, porque antes la clave la hacian la combinacion del idAnuncio e idUser, lo que implicaria un solo comentario por anuncio de cada usuario, añadido el campo que faltaba de comentario padre para gestionar las respuestas; La tabla imagenes tiene un atributo mas, porque en un anuncio puede haber mas de una imagen;  La tabla anuncio se ha unificado con la de producto, porque acordamos que los anuncio eran unicos, un anuncio es un producto, y no se venden por lotes ni tienen mas de una unidad. Al fin y al cabo cada animal es unico. He añadido los atributos que le faltan, como precio (0 por defecto), fecha, descripcion, localizacion, telefono, estado (activo, vendido, borrado/cancelado), idComprador, titulo...

--- bbdd/chargeCategories.sql
+++ bbdd/chargeCategories.sql
@@ -1,6 +1,6 @@
-INSERT INTO final_categoria VALUES ('Gatos');
-INSERT INTO final_categoria VALUES ('Perros');
-INSERT INTO final_categoria VALUES ('Peces');
-INSERT INTO final_categoria VALUES ('Pajaros');
-INSERT INTO final_categoria VALUES ('Reptiles');
-INSERT INTO final_categoria VALUES ('Caballos');
+INSERT INTO final_categoria VALUES (1, 'Gatos');
+INSERT INTO final_categoria VALUES (2, 'Perros');
+INSERT INTO final_categoria VALUES (3, 'Peces');
+INSERT INTO final_categoria VALUES (4, 'Pajaros');
+INSERT INTO final_categoria VALUES (5, 'Reptiles');
+INSERT INTO final_categoria VALUES (6, 'Caballos');
--- bbdd/createTable.sql
+++ bbdd/createTable.sql
@@ -1,59 +1,102 @@
+/* 
+*  TABLA USUARIO: 
+*  Guardar los usuario de la web
+*  El nick es unico, el nombre real no es necesario al registrarse
+*/
 CREATE TABLE final_usuario(
 	idUser INT(10) UNSIGNED AUTO_INCREMENT,
-	user VARCHAR(30) NOT NULL,
-	name VARCHAR(30) NOT NULL,
+	nick VARCHAR(30) NOT NULL,
+	name VARCHAR(40),
 	password VARCHAR(50) NOT NULL,	
 	email VARCHAR(30) NOT NULL,
 	CONSTRAINT PK_usuario PRIMARY KEY (idUser),
-	CONSTRAINT U_usuario UNIQUE (user)	
+	CONSTRAINT U_usuario UNIQUE (nick)	
 );
 
+/* 
+*  TABLA CATEGORIA: 
+*  Guardar las categorias a las que pueden pertenecer los anuncios
+*  La categoria otros es inborrable y siempre esta, asi que la metemos ya.
+*/
 CREATE TABLE final_categoria(
+    idCategoria INT(5) UNSIGNED AUTO_INCREMENT,
 	categoria VARCHAR(30) NOT NULL,
-	CONSTRAINT PK_categoria PRIMARY KEY (categoria)
+	CONSTRAINT PK_categoria PRIMARY KEY (idCategoria)
 );
 
+INSERT INTO final_categoria VALUES(0, 'Otros');
+
+/* 
+*  TABLA ANUNCIO: 
+*  El anuncio pertenece a un usuario y una categoria
+*  El anuncio tiene un precio una fecha.
+*  Si se borra el dueño del anuncio se borra el anuncio. No tiene sentido mantenerlo.
+*  Estado: 1 (activo), 0(borrado/cancelado), 2(vendido). Solo los anuncios estado 1 se muestran al usuario normal
+*  ¡¡¡ InnoDB no permite ON DELETE SET DEFAULT por lo que cuando queramos borrar
+*  una categoria en PHP primero hay que manualmente con php cambiar la categoria de todos
+*  esos anuncios a 0!!!
+*/
 CREATE TABLE final_anuncio(
 	idAnuncio INT(10) UNSIGNED AUTO_INCREMENT,
 	idUser INT(10) UNSIGNED NOT NULL,
-	categoria VARCHAR(30) NOT NULL,	
+	idCategoria INT(5) UNSIGNED DEFAULT 0,
+    fecha DATETIME DEFAULT CURRENT_TIMESTAMP,
+    precio INT(10) DEFAULT 0,
+    titulo VARCHAR(50) NOT NULL,
+    descripcion VARCHAR(2000),
+    localizacion VARCHAR(50) NOT NULL,
+    telefono VARCHAR(15), 
+    estado INT(1) DEFAULT 1,
+    idComprador INT(10) UNSIGNED,
 	CONSTRAINT PK_anuncio PRIMARY KEY (idAnuncio),
-	CONSTRAINT FK_anuncio_categoria FOREIGN KEY (categoria) references final_categoria(categoria),
-	CONSTRAINT FK_anuncio_usuario FOREIGN KEY (idUser) references final_usuario (idUser)
+	CONSTRAINT FK_anuncio_categoria FOREIGN KEY (idCategoria) references final_categoria(idCategoria),
+	CONSTRAINT FK_anuncio_usuario FOREIGN KEY (idUser) references final_usuario (idUser) ON DELETE CASCADE,
+    CONSTRAINT FK_anuncio_usuario2 FOREIGN KEY (idComprador) references final_usuario (idUser) ON DELETE SET NULL
 );
 
-CREATE TABLE final_producto(
-	idProducto INT(10) UNSIGNED AUTO_INCREMENT,
-	idUser INT(10) UNSIGNED NOT NULL,
-	idAnuncio INT(10) UNSIGNED NOT NULL,
-	CONSTRAINT PK_producto PRIMARY KEY (idProducto),
-	CONSTRAINT FK_producto_usuario FOREIGN KEY (idUser) references final_usuario (idUser),
-	CONSTRAINT FK_producto_anuncio FOREIGN KEY (idAnuncio) references final_anuncio (idAnuncio)
-);
-
+/* 
+*  TABLA COMENTARIO: 
+*  El comentario pertenece a un anuncio y un usuario
+*  El comentario tiene un maximo de 400 caracteres
+*  El comentario puede hacer referencia a otro comentario
+*/
 CREATE TABLE final_comentario(
+    idComentario INT(10) UNSIGNED AUTO_INCREMENT,
 	idUser INT(10) UNSIGNED NOT NULL,
 	idAnuncio INT(10) UNSIGNED NOT NULL,
-	comentario VARCHAR(200) NOT NULL,
-	CONSTRAINT PK_comentario PRIMARY KEY (idUser,idAnuncio),
-	CONSTRAINT FK_comentario_usuario FOREIGN KEY (idUser) references final_usuario(idUser),
-	CONSTRAINT FK_comentario_anuncio FOREIGN KEY (idAnuncio) references final_anuncio(idAnuncio)
+	comentario VARCHAR(400) NOT NULL,
+    idPadre INT(10) UNSIGNED,
+    fecha DATETIME DEFAULT CURRENT_TIMESTAMP,
+	CONSTRAINT PK_comentario PRIMARY KEY (idComentario),
+	CONSTRAINT FK_comentario_usuario FOREIGN KEY (idUser) references final_usuario(idUser) ON DELETE CASCADE,
+	CONSTRAINT FK_comentario_anuncio FOREIGN KEY (idAnuncio) references final_anuncio(idAnuncio) ON DELETE CASCADE,
+    CONSTRAINT FK_comentario_comentario FOREIGN KEY (idPadre) references final_comentario(idComentario) ON DELETE CASCADE
 );
 
+/* 
+*  TABLA FAVORITO: 
+*  El favorito se compone de un usuario y un anuncio
+*/
 CREATE TABLE final_favorito(
 	idUser INT(10) UNSIGNED NOT NULL,
 	idAnuncio INT(10) UNSIGNED NOT NULL,
-	CONSTRAINT PK_comentario PRIMARY KEY (idUser,idAnuncio),
-	CONSTRAINT FK_favorito_usuario FOREIGN KEY (idUser) references final_usuario(idUser),
-	CONSTRAINT FK_favorito_anuncio FOREIGN KEY (idAnuncio) references final_anuncio(idAnuncio)	
+	CONSTRAINT PK_favorito PRIMARY KEY (idUser,idAnuncio),
+	CONSTRAINT FK_favorito_usuario FOREIGN KEY (idUser) references final_usuario(idUser) ON DELETE CASCADE,
+	CONSTRAINT FK_favorito_anuncio FOREIGN KEY (idAnuncio) references final_anuncio(idAnuncio) ON DELETE CASCADE
 );
 
+/* 
+*  TABLA IMAGEN: 
+*  La imagen pertenece a un usuario, y tiene un id 
+*  (En un anuncio puede haber mas de una imagen)
+*  En los atributos se guarda el path a la imagen
+*/
 CREATE TABLE final_imagen(
 	idAnuncio INT(10) UNSIGNED NOT NULL,
-	original VARCHAR(100) NOT NULL,
+	idImagen INT(4) NOT NULL,
 	big VARCHAR(100) NOT NULL,
 	medium VARCHAR(100) NOT NULL,
 	small VARCHAR(100) NOT NULL,
-	CONSTRAINT PK_imagen PRIMARY KEY (idAnuncio),
-	CONSTRAINT FK_imagen_anuncio FOREIGN KEY (idAnuncio) references final_anuncio (idAnuncio)
+	CONSTRAINT PK_imagen PRIMARY KEY (idAnuncio, idImagen),
+	CONSTRAINT FK_imagen_anuncio FOREIGN KEY (idAnuncio) references final_anuncio (idAnuncio) ON DELETE CASCADE
 );
